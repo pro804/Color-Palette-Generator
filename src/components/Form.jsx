e4974d3a829105451c7eb7ae0516bb09448d69b1
pro804 <petsoulis.appdev@gmail.com>
2025-09-15 12:54:22 +0300

    feat: add color input and synchronize form inputs - Implement HTML color picker input in Form component - Synchronize color state between color picker and text input - Update button background color dynamically based on selected color - Prepare handleSubmit function for form submission

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,11 +2,19 @@ import { useState } from "react";
 
 const Form = () => {
   const [color, setColor] = useState("");
+  const handleSubmit = (e) => {};
 
   return (
     <section className="container">
       <h4>Color Palette Generator</h4>
-      <form className="color-form">
+      <form className="color-form" onSubmit={handleSubmit}>
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => {
+            setColor(e.target.value);
+          }}
+        />
         <input
           type="text"
           value={color}
